1f7e37fa1593b65e17f77e575b170e8e38888b41
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 23:14:28 +0530

    g

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Review from './Review';
+import React, { useState } from 'react';
 
 const reviewsData = [
   {
@@ -41,10 +40,48 @@ const reviewsData = [
 ];
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = reviewsData[index];
+
+  const nextReview = () => {
+    setIndex((prevIndex) => {
+      const newIndex = prevIndex === reviewsData.length - 1 ? 0 : prevIndex + 1;
+      return newIndex;
+    });
+  };
+
+  const prevReview = () => {
+    setIndex((prevIndex) => {
+      const newIndex = prevIndex === 0 ? reviewsData.length - 1 : prevIndex - 1;
+      return newIndex;
+    });
+  };
+
+  const randomReview = () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * reviewsData.length);
+    } while (newIndex === index);
+
+    setIndex(newIndex);
+  };
+
   return (
     <section className="container">
       <h2 id="review-heading">Our Reviews</h2>
-      <Review reviewsData={reviewsData} />
+      <div className="review">
+        <div className="person-img">
+          <img src={image} alt={name} />
+        </div>
+        <h4 className="author" id={`author-${reviewsData[index].id}`}>{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div>
+          <button className="prev-btn" onClick={prevReview}>Prev</button>
+          <button className="next-btn" onClick={nextReview}>Next</button>
+        </div>
+        <button className="random-btn" onClick={randomReview}>Surprise Me</button>
+      </div>
     </section>
   );
 }
